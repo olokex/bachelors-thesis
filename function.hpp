4bc69f2f5e746cfcf9ac5405a4fc72801a896adf
olok <olokex@gmail.com>
2020-09-05 07:08:49 +0200

    header file with function-gates

---
+++ function.hpp
@@ -0,0 +1,16 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+enum class Function {
+    In,
+    Not,
+    And,
+    Or,
+    Xor,
+    Nand,
+    Nor,
+    Xnor,
+    NumberOfFunctions
+};
+
+#endif /* FUNCTION_H */
