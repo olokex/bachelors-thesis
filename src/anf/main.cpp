837998e96fa5c6c6c9770d181af9f483d3711840
olok <olokex@gmail.com>
2020-10-03 16:33:22 +0200

    some new training circuits (truth tables - files), changed iterations to generations nothing serious

--- src/anf/main.cpp
+++ src/anf/main.cpp
@@ -23,10 +23,10 @@ void evolution(const Parameters &p, const ReferenceBits &ref) {
     for (unsigned int gen = 0; gen < p.generations; gen++) {
         for (int i = 0; i < p.lambda; i++) {
             population[i] = fittest;
-            //population[i].mutate_overall(p.mutation, ref);
+            population[i].mutate_overall(p.mutation, ref);
             //population[i].mutate_in_row(p.mutation_row, ref);
             //population[i].mutate_uniform(ref, p.uniform_mutation);
-            population[i].mutate_overall(p.mutation, ref);
+            //population[i].mutate_overall(p.mutation, ref);
             //population[i].crossover(fittest);
             population[i].calculate_fitness(p, ref);
             if (population[i].fitness == 0) {
@@ -43,7 +43,7 @@ void evolution(const Parameters &p, const ReferenceBits &ref) {
             fittest = new_fittest;
         }
         if (gen % p.print_count == 0 && p.print_fitness) {
-            std::cout << "iter: " << gen << " fitness: " << fittest.fitness << std::endl;
+            std::cout << "generation: " << gen << " best fitness: " << fittest.fitness << std::endl;
         }
     }
     std::cout << "NOT FOUND" << std::endl;
@@ -53,8 +53,7 @@ int main(int argc, char *argv[]) {
     try {
         Parameters p(argc, argv);
         ReferenceBits ref(p.path);
-        p.is_valid(ref);
-        evolution(p, ref);
+        p.is_valid(ref);        evolution(p, ref);
     } catch (const std::runtime_error &err) {
         std::cerr << "ERROR: " << err.what() << std::endl;
         return 1;
